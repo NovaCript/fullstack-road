6090a916430a3fa905aecf53aa3f6b3241d8ff05
Gleb I <novacript.dev@gmail.com>
2026-01-12 09:25:47 +0200

    add type and math practic

---
+++ 005-math/demo-random.js
@@ -0,0 +1,3 @@
+let rand = Math.random();
+rand = Math.round(rand * 10);
+console.log(rand);
---
+++ 005-math/info.js
@@ -0,0 +1,27 @@
+// Math - object
+
+// константы
+console.log(`Константа Пи: ${Math.PI}`);
+console.log(`Константа е: ${Math.E}`);
+
+console.log(`Абсолютное значение -10: ${Math.abs(-10)}`);
+console.log(`Квадратный корень из 16: ${Math.sqrt(16)}`);
+console.log(`2 в степени 3: ${Math.pow(2,3)}`);
+console.log(`Отсечение дробной части 4.6: ${Math.trunc(4.6)}`);
+console.log(`Округление вверх 4.3: ${Math.ceil(4.3)}`);
+console.log(`Округление вниз 4.7: ${Math.floor(4.7)}`);
+console.log(`Округление 4.5: ${Math.round(4.5)}`);
+console.log(`Псевдослучайное число от 0 до 1: ${Math.random()}`);
+
+// Тригонометрические функции
+console.log(`Синус угла пи радиан: ${Math.sin(Math.PI)}`);
+console.log(`Косинус угла пи радиан: ${Math.cos(Math.PI)}`);
+console.log(`Тангенс угла пи/4 радиан: ${Math.tan(Math.PI / 4)}`);
+
+// Логарифмические функции
+console.log(`Натуральный логарифм числа e: ${Math.log(Math.E)}`);
+console.log(`Десятичный логарифм числа 100: ${Math.log10(100)}`);
+
+// Прочие функции
+console.log(`Минимальное число из (1, 2, 3): ${Math.min(1, 2, 3)}`);
+console.log(`Максимальное число из (1, 2, 3): ${Math.max(1, 2, 3)}`);
