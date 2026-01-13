256f04fa776b07caaaac43a2218dd3d78caf30f9
Gleb I <novacript.dev@gmail.com>
2026-01-13 21:26:54 +0200

    fixing logical errors in a function tipCalculator

--- 007-tip-calculator/ex.js
+++ 007-tip-calculator/ex.js
@@ -1,11 +1,15 @@
 function tipCalculator(person, total, tip) {
+  let totalSum = total + (total * tip / 100);
+  let costForPerson = totalSum / person;
   console.log(`
     -----------------------------------------
-        Счет на сумму ${total * 1.1}
+        Счет на сумму ${totalSum} руб
         Чаевые: ${tip}%
-        С каждого человека по ${(total * 1.1) / person}
+        С каждого человека по ${costForPerson} руб
     -----------------------------------------    
         `);
 }
 
 tipCalculator(4, 1000, 10);
+tipCalculator(8, 888, 5);
+tipCalculator(2, 2000, 10);
