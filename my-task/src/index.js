7b42350aea4e60ea8a09b4a440abb46c00542d81
Gleb I <novacript.dev@gmail.com>
2026-01-24 05:17:46 +0300

    bootstrap inclusion

--- my-task/src/index.js
+++ my-task/src/index.js
@@ -2,8 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-
-function getTaskRender (array) {
+function getTaskRender(array) {
   return (
     <ul>
       {array.map((element) => (
@@ -19,19 +18,44 @@ const person = {
 };
 
 const header = "Какой то заголовок";
-const task = ["Умыться", "Выпить кофе", "Посидеть у моря", "Обучение", "Поужинать",];
+const tasks = [
+  "Умыться",
+  "Выпить кофе",
+  "Посидеть у моря",
+  "Обучение",
+  "Поужинать",
+];
 const currentYear = new Date().getFullYear();
 const footer = `© Авторское право дает право на авторское право ${currentYear}`;
-const taskList = getTaskRender(task);
+const taskList = getTaskRender(tasks);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <div>
+  <div className="container col-10">
     <h1>{header}</h1>
     <h2>
       {person.firstName} {person.lastName}
     </h2>
     {taskList}
+    {
+      // tasks.map(task => (<li>{task}</li>))
+      Array.from({ length: 5 }, (_, index) => (
+        <div className="card my-5">
+          <img
+            src={`https://picsum.photos/200/100?random=${index}`}
+            class="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card’s content.
+            </p>
+          </div>
+        </div>
+      ))
+    }
     <footer style={{ position: "fixed", bottom: 0, width: "100%" }}>
       {footer}
     </footer>
