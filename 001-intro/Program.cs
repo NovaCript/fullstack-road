068f2b6eec9291db93d73ffb720f08453511e8a7
Gleb I <novacript.dev@gmail.com>
2026-01-26 11:26:26 +0300

    strict project

---
+++ 001-intro/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Hello, World!");
+
+int age;
+double salary;
+string name;
+char c;
+
+age = 30;
+salary = 2500.5;
+name = "John";
+c = 'c';
+
+string productName = "Ноутбук";
+double price = 1200.99;
+int quantityInStock = 50;
+char a = 'a';
+
+// productName != ProductName
+
+Console.WriteLine(productName + " " + price);
