c8ce22b1e33796a0ebfaa53d4bdffb23be557e7a
Gleb I <novacript.dev@gmail.com>
2026-01-26 12:06:23 +0300

    type var

---
+++ 004-data-types/Program.cs
@@ -0,0 +1,17 @@
+﻿int integerNumber = 42;
+
+double floatingPointNumber = 3.14;
+
+bool boolearValue = true;
+
+char character = 'A';
+
+string name = "John";
+
+// Особый тип данных
+object anyType = "Это строка";
+anyType = 10; // теперь число
+
+Week today = Week.Monday;
+System.Console.WriteLine(today);
+enum Week { Monday, Tuesday, Wednesday }
