15808d3bde54e8a08bfcff6843a6df6440852556
Gleb I <novacript.dev@gmail.com>
2026-01-27 16:12:04 +0300

    add some lesson

--- 004-data-types/Program.cs
+++ 004-data-types/Program.cs
@@ -1,4 +1,6 @@
-﻿int integerNumber = 42;
+﻿using System.Dynamic;
+
+int integerNumber = 42;
 
 double floatingPointNumber = 3.14;
 
@@ -12,6 +14,55 @@ string name = "John";
 object anyType = "Это строка";
 anyType = 10; // теперь число
 
+Point p1 = new Point(10, 20);
+p1.Display();
+
+Person person = new Person("Gleb", 31);
+person.SayHello();
+System.Console.WriteLine($"{person.Age}, {person.Name}");
+person.Name = "Semen";
+System.Console.WriteLine(person.Name);
+
+
 Week today = Week.Monday;
 System.Console.WriteLine(today);
 enum Week { Monday, Tuesday, Wednesday }
+
+public class Person
+{
+    private string name;
+    private int age;
+
+    public Person(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+
+    public string Name
+    {
+        get => name;
+        set => name = value;
+    }
+
+    public int Age { get => age;}
+
+    public void SayHello()
+    {
+        Console.WriteLine($"Hello my name is {name}, my age is {age}");
+    }
+
+}
+public struct Point
+{
+    public int X;
+    public int Y;
+
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+    public void Display() => Console.WriteLine("$X: {X}, Y: {Y}");
+
+}
---
+++ 005-math/Program.cs
@@ -0,0 +1,28 @@
+﻿// Абсолютное значение
+System.Console.WriteLine(Math.Abs(-5));
+// Максимальное и минимальное
+System.Console.WriteLine(Math.Max(5, -6));
+System.Console.WriteLine(Math.Min(5, -6));
+// Округление
+System.Console.WriteLine(Math.Ceiling(2.5)); // до большего числа
+System.Console.WriteLine(Math.Floor(2.8)); // до меньшего числа
+System.Console.WriteLine(Math.Round(2.51)); // до ближайшего целого
+
+// Тригонометрические функции
+double angle = Math.PI / 4; // 45 градусов в радианах
+System.Console.WriteLine(Math.Sin(angle));
+System.Console.WriteLine(Math.Cos(angle));
+System.Console.WriteLine(Math.Tan(angle));
+
+// экспоненциальные и логарифмические функции
+System.Console.WriteLine(Math.Exp(2));
+System.Console.WriteLine(Math.Log(Math.E));
+System.Console.WriteLine(Math.Log10(100));
+
+// степени функций
+System.Console.WriteLine(Math.Pow(2,3));
+System.Console.WriteLine(Math.Sqrt(16));
+
+// вывод констант
+System.Console.WriteLine(Math.PI);
+System.Console.WriteLine(Math.E);
