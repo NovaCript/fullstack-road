6bfdb4d98717c7b8784d16e70b919273a5a94b2e
Gleb I <novacript.dev@gmail.com>
2026-01-28 16:29:34 +0300

    logic lesson

---
+++ 009-logic/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = 5, b = 5;
+Console.WriteLine($"{a} равно {b}: {a == b}");
+
+int c = 5, d = 3;
+Console.WriteLine($"{c} не равно {d}: {c != d}");
+
+
+bool res = (a > b) && (c < d);
+Console.WriteLine($"{res}");
+
+res = (a > b) || (c > d);
+Console.WriteLine($"{res}");
+
+
+// Пример использования логического исключающее ИЛИ (XOR)
+bool m = true, n = false;
+Console.WriteLine($"Только одно из условий истинно: {m ^ n}");
